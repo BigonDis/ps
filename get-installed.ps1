8b037369b958d030617389fbdd9ca406d9efcb7e
Lee W. Abner <labner@cibdp.com>
2017-11-11 20:55:48 -0600

    initial commit of ps scripts

---
+++ get-installed.ps1
@@ -0,0 +1,8 @@
+﻿#-- Get listing of all installed applications --#
+
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize
+
+#-- List all Store Installed Applications --#
+
+Get-AppxPackage | Select-Object Name, PackageFullName, Version | Format-Table -AutoSize
---
+++ setup-dhcp.ps1
@@ -0,0 +1,11 @@
+﻿#-- Install DHCP --#
+
+Get-WindowsFeature | Where-Object Name -like *dhcp*Install-WindowsFeature DHCP -IncludeManagementTools
+
+#-- Create Scopes --#
+ 
+Add-DhcpServerv4Scope -Name "Datacenter" -StartRange 10.111.13.1 -EndRange 10.111.13.64 -SubnetMask 255.255.248.0
+
+#-- Set DHCP Options --#
+
+Set-DhcpServerv4OptionValue -ScopeID 10.111.8.0 -DnsDomain cibdp.com -DnsServer 10.111.9.25,10.111.9.22 -Router 10.111.9.2
