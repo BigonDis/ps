c1f8a50086f6173595c409dd97d3918671ecf4ad
Lee W. Abner <labner@cibdp.com>
2017-11-23 16:46:25 -0600

    added scrupt for finding snapshots

---
+++ find-snaps.ps1
@@ -0,0 +1,12 @@
+ 
+#-- Import Module for Powercli --#
+Import-Module -Name VMware.Powercli
+
+#-- Connect to Virtual Center Server --#
+Connect-VIServer -Server oakvc.cibdp.com
+
+#-- Get Snapshots and Export to CSV File --#
+get-vm | Get-Snapshot | select vm, name, created | export-csv c:\users\oman\Documents\snaps.csv
+
+#-- Email Report out --#
+Send-MailMessage -To "Lee.Abner@cibmis.com" -From "admin@cibmis.com" -SMTPServer "exchange.cibdp.com" -Subject "Snapshot Report" -Attachments c:\users\oman\Documents\snaps.csv
